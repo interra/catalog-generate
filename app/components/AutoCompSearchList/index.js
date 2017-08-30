324b7d2858416f83da40ad153168e7d9a1a95fe9
acouch <acinternets@gmail.com>
2017-08-30 01:13:50 -0400

    Updates demos

--- app/components/AutoCompSearchList/index.js
+++ app/components/AutoCompSearchList/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import List from 'components/List';
+import ListItem from 'components/ListItem';
 import LoadingIndicator from 'components/LoadingIndicator';
 import HomeListItem from 'components/HomeListItem';
 import H3 from './H3';
